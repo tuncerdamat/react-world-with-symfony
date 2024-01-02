831577603c8035f7b8d0793ee745bb0f55f2b782
tuncerdamat <tuncer.damat@gmail.com>
2024-01-02 17:26:10 +0100

    JSX is a fancy way of creating react element objects

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom'; 
+import ReactDom from 'react-dom';
 
-const el = React.createElement(
-    'h2', 
-    null,
-    'Lift History'
-);
+const el = <h2>Lift Stuff! <span>❤️</span></h2>;
 
 ReactDom.render(el, document.getElementById('lift-stuff-app'));
