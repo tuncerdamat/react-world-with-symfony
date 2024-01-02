1b627df34072508f4d55080249002571049c2183
tuncerdamat <tuncer.damat@gmail.com>
2024-01-02 18:05:01 +0100

    Mastering Props

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -2,4 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';  // that means we only need render function from react-dom, not the whole module
 import RepLogApp from "./RepLog/RepLogApp";
 
-render(<RepLogApp/>, document.getElementById('lift-stuff-app'));
+const shouldShowHeart = true;
+
+render(
+    <diV>
+        <RepLogApp withHeart={shouldShowHeart}/>
+        <RepLogApp withHeart={false}/>
+    </diV>,
+    document.getElementById('lift-stuff-app')
+);
