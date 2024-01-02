a25fd86091219134bf517e02c8f708d16668b40d
tuncerdamat <tuncer.damat@gmail.com>
2024-01-02 17:18:20 +0100

    Creating the first React element with React.createElement

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,10 @@
-console.log("Hello react reps");
+import React from 'react';
+import ReactDom from 'react-dom'; 
+
+const el = React.createElement(
+    'h2', 
+    null,
+    'Lift History'
+);
+
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
