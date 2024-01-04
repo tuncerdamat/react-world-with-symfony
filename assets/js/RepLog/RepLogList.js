248836133a1d03d1770d0f75ffaf64e8caf70d1a
tuncerdamat <tuncer.damat@gmail.com>
2024-01-04 18:06:00 +0100

    Smart vs Dumb Components

--- assets/js/RepLog/RepLogList.js
+++ assets/js/RepLog/RepLogList.js
@@ -1,32 +1,30 @@
-import React, {Component} from "react";
+import React from "react";
 
-export default class RepLogList extends Component {
-    render(){
-        const { highlightedRowId, onRowClick } = this.props;
-        
-        const repLogs = [
-            { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
-            { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
-            { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
-        ];
-        
-        return (
-            <tbody>
-            { repLogs.map((repLog) =>
-                (
-                    <tr key={repLog.id}
-                        className={this.props.highlightedRowId === repLog.id ? 'info' : ''}
-                        onClick={(event) => onRowClick(repLog.id)}
-                    >
-                        <td>{repLog.itemLabel}</td>
-                        <td>{repLog.reps}</td>
-                        <td>{repLog.totalWeightLifted}</td>
-                        <td>...</td>
-                    </tr>
-                )
+export default function RepLogList(props) {
+    const {highlightedRowId, onRowClick} = props;
+
+    const repLogs = [
+        {id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5},
+        {id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180},
+        {id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72}
+    ];
+
+    return (
+        <tbody>
+        {repLogs.map((repLog) =>
+            (
+                <tr key={repLog.id}
+                    className={props.highlightedRowId === repLog.id ? 'info' : ''}
+                    onClick={(event) => onRowClick(repLog.id)}
+                >
+                    <td>{repLog.itemLabel}</td>
+                    <td>{repLog.reps}</td>
+                    <td>{repLog.totalWeightLifted}</td>
+                    <td>...</td>
+                </tr>
             )
-            }
-            </tbody>
-        );
-    }
+        )
+        }
+        </tbody>
+    );
 }
