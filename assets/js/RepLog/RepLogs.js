7953c1296e82b89eec3b248532b4979b70096c26
tuncerdamat <tuncer.damat@gmail.com>
2024-01-10 15:08:20 +0100

    New ReplogCreator component to hold the form

--- assets/js/RepLog/RepLogs.js
+++ assets/js/RepLog/RepLogs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import RepLogList from "./RepLogList";
 import PropTypes from "prop-types";
+import RepLogCreator from "./RepLogCreator";
 
 const calculateTotalWeightLiftedFancier = repLogs => repLogs.reduce((total, log) => total + log.totalWeightLifted, 0);
 
@@ -10,15 +11,6 @@ export default function RepLogs(props) {
     let heart = '';
     if(withHeart){
         heart = <span>❤️</span>;
-    }
-    
-    function handleFormSubmit(event){
-        event.preventDefault();
-
-        console.log('Form submited');
-        console.log(event.target.elements.namedItem('reps').value);
-        
-        onNewItemSubmit('Big fat cat', event.target.elements.namedItem('reps').value);
     }
 
     return (
@@ -47,36 +39,8 @@ export default function RepLogs(props) {
                 </tr>
                 </tfoot>
             </table>
-
-            <form className="form-inline" onSubmit={handleFormSubmit}>
-                <div className="form-group">
-                    <label className="sr-only control-label required" htmlFor="rep_log_item">
-                        What did you lift?
-                    </label>
-                    <select id="rep_log_item"
-                            name="item"
-                            required="required"
-                            className="form-control">
-                        <option value="">What did you lift?</option>
-                        <option value="cat">Cat</option>
-                        <option value="fat_cat">Big Fat Cat</option>
-                        <option value="laptop">My Laptop</option>
-                        <option value="coffee_cup">Coffee Cup</option>
-                    </select>
-                </div>
-                {' '}
-                <div className="form-group">
-                    <label className="sr-only control-label required" htmlFor="rep_log_reps">
-                        How many times?
-                    </label>
-                    <input type="number" id="rep_log_reps"
-                           name="reps" required="required"
-                           placeholder="How many times?"
-                           className="form-control"/>
-                </div>
-                {' '}
-                <button type="submit" className="btn btn-primary">I Lifted it!</button>
-            </form>
+            
+            <RepLogCreator onNewItemSubmit={onNewItemSubmit} />
         </div>
     );
 }
