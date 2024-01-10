08da4ca00cd430cc868ba2c21dd1e1a01ed97570
tuncerdamat <tuncer.damat@gmail.com>
2024-01-10 13:14:18 +0100

    Moving the Rep Logs to State

--- assets/js/RepLog/RepLogs.js
+++ assets/js/RepLog/RepLogs.js
@@ -3,7 +3,7 @@ import RepLogList from "./RepLogList";
 import PropTypes from "prop-types";
 
 export default function RepLogs(props) {
-    const { withHeart, highlightedRowId, onRowClick } = props;
+    const { withHeart, highlightedRowId, onRowClick, repLogs } = props;
     
     let heart = '';
     if(withHeart){
@@ -25,6 +25,7 @@ export default function RepLogs(props) {
                 <RepLogList
                     highlightedRowId={highlightedRowId}
                     onRowClick={onRowClick}
+                    repLogs={repLogs}
                 />
                 <tfoot>
                 <tr>
@@ -72,5 +73,6 @@ export default function RepLogs(props) {
 RepLogs.propTypes = {
     withHeart: PropTypes.bool,
     highlightedRowId: PropTypes.any,
-    onRowClick: PropTypes.func.isRequired
+    onRowClick: PropTypes.func.isRequired,
+    repLogs: PropTypes.array.isRequired,
 }
