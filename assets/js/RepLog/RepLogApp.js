1903ec9b817f96100be3366b8952a765b8741d69
tuncerdamat <tuncer.damat@gmail.com>
2024-01-12 12:19:57 +0100

    Working with refs

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import RepLogs from "./RepLogs";
 import PropTypes from "prop-types";
+import { v4 as uuid } from 'uuid';
 
 export default class RepLogApp extends Component {
     constructor(props) {
@@ -9,22 +10,31 @@ export default class RepLogApp extends Component {
         this.state = {
             highlightedRowId: null,
             repLogs: [
-                { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
-                { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
-                { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+                { id: uuid(), reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
+                { id: uuid(), reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+                { id: uuid(), reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
             ]
         };
         
         this.handleRowClick = this.handleRowClick.bind(this);
+        this.handleNewItemSubmit = this.handleNewItemSubmit.bind(this);
     }
     
     handleRowClick(repLogId){
         this.setState({highlightedRowId: repLogId});
     }
     
-    handleNewItemSubmit(itemName, reps){
-        console.log('TODO - handle');
-        console.log(itemName, reps);
+    handleNewItemSubmit(itemLabel, reps){
+        const repLogs = this.state.repLogs;
+        const newRep = {
+            id: uuid(),
+            reps: reps,
+            itemLabel: itemLabel,
+            totalWeightLifted: Math.floor(Math.random() * 50),
+        }
+        
+        repLogs.push(newRep);
+        this.setState({repLogs: repLogs});
     }
     
     render () {
--- assets/js/RepLog/RepLogCreator.js
+++ assets/js/RepLog/RepLogCreator.js
@@ -5,17 +5,23 @@ import RepLogs from "./RepLogs";
 export default class RepLogCreator extends Component {
     constructor(props) {
         super(props);
-        
+        this.quantityInput = React.createRef()
+        this.itemSelect = React.createRef()
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
     }
     handleFormSubmit(event) {
         event.preventDefault();
         const { onNewItemSubmit } = this.props;
+        const quantityInput = this.quantityInput.current;
+        const itemSelect = this.itemSelect.current;
     
-        console.log('Form submited');
-        console.log(event.target.elements.namedItem('reps').value);
-    
-        onNewItemSubmit('Big fat cat', event.target.elements.namedItem('reps').value);
+        onNewItemSubmit(
+            itemSelect.options[itemSelect.selectedIndex].text,
+            quantityInput.value
+        );
+        
+        quantityInput.value = '';
+        itemSelect.selectedIndex = 0;
     }
 
     render() {
@@ -26,7 +32,7 @@ export default class RepLogCreator extends Component {
                         What did you lift?
                     </label>
                     <select id="rep_log_item"
-                            name="item"
+                            ref={this.itemSelect}
                             required="required"
                             className="form-control">
                         <option value="">What did you lift?</option>
@@ -41,8 +47,10 @@ export default class RepLogCreator extends Component {
                     <label className="sr-only control-label required" htmlFor="rep_log_reps">
                         How many times?
                     </label>
-                    <input type="number" id="rep_log_reps"
-                           name="reps" required="required"
+                    <input type="number" 
+                           id="rep_log_reps"
+                           ref={this.quantityInput}
+                           required="required"
                            placeholder="How many times?"
                            className="form-control"/>
                 </div>
