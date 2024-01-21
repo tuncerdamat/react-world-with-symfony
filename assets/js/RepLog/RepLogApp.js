45a56bc07957976ec989e10366f8778c82f7303c
tuncerdamat <tuncer.damat@gmail.com>
2024-01-21 11:09:27 +0100

    Hook up the DELETE link

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import RepLogs from "./RepLogs";
 import PropTypes from "prop-types";
 import { v4 as uuid } from 'uuid';
-import { getRepLogs } from "../api/rep_log_api";
+import { getRepLogs, deleteReplog } from "../api/rep_log_api";
 
 export default class RepLogApp extends Component {
     constructor(props) {
@@ -56,6 +56,8 @@ export default class RepLogApp extends Component {
     }
     
     handleDeleteRepLog(id){
+        deleteReplog(id)
+        
         // remove the rep log without mutating state
         // filter returns a new array
         this.setState((prevState) => {
--- assets/js/api/rep_log_api.js
+++ assets/js/api/rep_log_api.js
@@ -1,13 +1,24 @@
+function fetchJson(url, options) {
+    return fetch(url, Object.assign({
+        credentials: 'same-origin'
+    }, options))
+        .then(response => {
+            return response.json()
+        })
+}
+
 /**
  * Returns a Promise object with the rep logs data
  * 
  * @returns {Promise<any>}
  */
 export function getRepLogs () {
-    return fetch('/reps', {
-        credentials: 'same-origin'
+    return fetchJson('/reps')
+        .then(data => data.items);
+}
+
+export function deleteReplog(id) {
+    return fetchJson(`/reps/${id}`, {
+        method: 'DELETE'
     })
-        .then((response) => {
-            return response.json().then((data) => data.items);
-        })
 }
