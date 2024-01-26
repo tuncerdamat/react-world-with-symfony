139f57900a20ada6db7468660256d4e4ef4502f4
tuncerdamat <tuncer.damat@gmail.com>
2024-01-26 12:45:05 +0100

    Server Validation & fetch Failing

--- assets/js/api/rep_log_api.js
+++ assets/js/api/rep_log_api.js
@@ -2,18 +2,30 @@ function fetchJson(url, options) {
     return fetch(url, Object.assign({
         credentials: 'same-origin'
     }, options))
+        .then(checkStatus)
         .then(response => {
             return response.text()
                 .then(text => text ? JSON.parse(text) : '')
         })
 }
 
+function checkStatus(response) {
+    if (response.status >= 200 && response.status < 400) {
+        return response
+    }
+
+    const error = new Error(response.statusText)
+    error.response = response
+
+    throw error
+}
+
 /**
  * Returns a Promise object with the rep logs data
- * 
+ *
  * @returns {Promise<any>}
  */
-export function getRepLogs () {
+export function getRepLogs() {
     return fetchJson('/reps')
         .then(data => data.items);
 }
@@ -25,11 +37,11 @@ export function deleteReplog(id) {
 }
 
 export function createRepLog(repLog) {
-       return fetchJson('/reps', {
-           method: 'POST',
-           body: JSON.stringify(repLog),
-           headers: {
-               'Content-Type': 'application/json',
-           }
-       })
+    return fetchJson('/reps', {
+        method: 'POST',
+        body: JSON.stringify(repLog),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
 }
